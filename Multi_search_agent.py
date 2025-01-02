6dc413cb96aa559fc27026b8ee62822bc7273900
pschoudhary-dot <pschoudhary.enacton@gmail.com>
2025-01-02 11:51:36 +0530

    refined the Multi_search_agent to use different tools like google search and sqldata to serach teh web and better generate a blogpost!!!

--- Multi_search_agent.py
+++ Multi_search_agent.py
@@ -1,9 +1,6 @@
-import httpx
 import os
-from phi.agent import Agent
+from phi.storage.agent.sqlite import SqlAgentStorage
 from phi.model.google import Gemini
-from pathlib import Path
-from phi.tools.csv_tools import CsvTools
 from phi.tools.googlesearch import GoogleSearch
 from phi.storage.agent.sqlite import SqlAgentStorage
 from phi.playground import Playground, serve_playground_app
@@ -35,18 +32,16 @@ web_search_agent=Agent(
     - By providing organized and digestible summaries, the AI Research Assistant enables content creators to focus on creative ideation, storytelling, and content development rather than spending excessive time on research and information gathering.
     """,
     tools=[GoogleSearch()],
-    storage=SqlAgentStorage(table_name="web_search_agent_sessions", db_file="tmp/agents.db"),
     markdown=True,
     model=Gemini(id="gemini-2.0-flash-exp"),
     add_datetime_to_instructions=True,
-    show_tool_calls=True,
-    add_chat_history_to_messages=True,
-    
+    show_tool_calls=True,    
 )
 
 content_creator_agent=Agent(
     role="Content Creator",
-    description="Your goal is to generate initial drafts of content based on templates, guidelines, and inputs for the research phase.",
+    tools=[GoogleSearch()],
+    description="you are a senior NYT editor and your task is to write the New yourk times worthy cover story. Your goal is to generate initial drafts of content based on templates, guidelines, and inputs for the research phase.",
     instructions="""
     Your goal is to generate initial drafts of content based on templates, guidelines, and inputs for the research phase.
     - **Creating Initial Content Drafts for Various Formats:** This involves using natural language generation (NLG) technologies to generate preliminary drafts of content in different formats, such as blog posts, articles, and social media updates. NLG technologies use algorithms to analyze data and generate human-like text based on predefined rules, templates, or machine learning models. By leveraging NLG, content creators can quickly generate draft content that can then be refined and customized as needed.
@@ -56,12 +51,12 @@ content_creator_agent=Agent(
     - **Generating Creative Ideas for Content:** In addition to generating draft content, content creators are responsible for brainstorming and developing creative ideas for content campaigns. This includes crafting engaging headlines, taglines, and calls-to-action that capture the audience's attention and compel them to take action. Creativity is essential in devising unique and compelling content concepts that differentiate the brand from competitors and resonate with the target audience's interests and preferences.
     """,
     model=Gemini(id="gemini-2.0-flash-exp"),
-    storage=SqlAgentStorage(table_name="content_creator_agent_session", db_file="tmp/agents.db"),
     show_tool_calls=True,
-    add_chat_history_to_messages=True,
+    add_datetime_to_instructions=True,
 )
 
 seo_analyst_agent = Agent(
+    tools=[GoogleSearch()],
     role="SEO Analyst",
     description="Optimizes content for search engines and improves content discoverability online.",
     instructions="""
@@ -75,7 +70,7 @@ seo_analyst_agent = Agent(
     model=Gemini(id="gemini-2.0-flash-exp"),
     storage=SqlAgentStorage(table_name="seo_analyst_agent_session", db_file="tmp/agents.db"),
     show_tool_calls=True,
-    add_chat_history_to_messages=True,
+    add_datetime_to_instructions=True,
 )
 
 editorial_assistant_agent = Agent(
@@ -92,70 +87,32 @@ editorial_assistant_agent = Agent(
     The Editorial Assistant performs thorough final reviews to verify the readiness of content for publication. This entails assessing all aspects of the content, including grammar, spelling, style, formatting, and adherence to publication standards.
     """,
     model=Gemini(id="gemini-2.0-flash-exp"),
-    storage=SqlAgentStorage(table_name="editorial_assistant_agent_session", db_file="tmp/agents.db"),
-    add_chat_history_to_messages=True,
+    add_datetime_to_instructions=True,
 )
 
 multi_agent=Agent(
     model=Gemini(id="gemini-2.0-flash-exp"),
     team=[web_search_agent, content_creator_agent, seo_analyst_agent, editorial_assistant_agent],
-    instructions=[
-    """Structuring a content plan that includes headings, subheadings, and key points to cover for the {topic}.
-
-    Exaple output:
-    ### Title: The Role of AI in Healthcare
-
-    ### Introduction:
-    - Brief overview of ....
-
-    ### 1: AI Applications in Diagnosis and Disease Detection
-    * 1.1: Medical Imaging and Radiology
-        * Key points:
-            - Use of AI algorithms to analyze medical images (X-rays, MRIs, CT scans) for early detection of diseases.
-            - Improved accuracy and efficiency in identifying abnormalities and diagnosing conditions such as cancer, fractures, and cardiovascular diseases.
-    * 1.2: .......
-
-    Also, give me some external website link.
-
-    ### Conclusion:
-    - Summary of the key roles and applications of AI in healthcare, from diagnosis and treatment to personalized medicine and remote monitoring.
-    - Discussion of the potential benefits and challenges of AI adoption in healthcare, along with considerations for future developments.
-    - Call-to-action encouraging readers to stay informed about advancements in AI healthcare and to engage in discussions about the ethical and regulatory implications.
-    - Closing remarks emphasizing the transformative potential of AI in improving patient care and shaping the future of healthcare delivery.
-    """,
-    
-    """
-     - Based on the content plan, generate a draft using an AI text-generation model. This should form the body of your content.
-    - Generating creative ideas for content, including headlines, taglines, and calls-to-action.
-    """,
-    
-    """ Apply SEO best practices to the generated content. This may involve:
-    - Keyword optimization.
-    - Meta tags and descriptions.
-    - Readability improvements.
-
-    The expected output of an SEO Analyst is analyzing content for SEO best practices, including keyword density, meta descriptions, and title tags. Recommending improvements to enhance content ranking on search engines. Monitoring content performance and providing insights for content optimization.
-    """,
-    
-    """The expected output is high-quality content that is polished, accurate, and in line with the publication's editorial standards, ensuring a positive reader experience and maintaining the publication's reputation for excellence.
-    """,
-    ],
     show_tool_calls=True,
     markdown=True,
-    add_chat_history_to_messages=True,
-    add_context_to_instructions=True,
-        
+    add_datetime_to_instructions=True,
+    #debug_mode=True,
+       
 )
 
-#multi_agent.print_response("Write a blog about Lammacoder!", stream=True,show_full_reasoning=True)
-app = Playground(
-    agents=[
-        web_search_agent, 
-        seo_analyst_agent, 
-        content_creator_agent, 
-        editorial_assistant_agent 
-        ]
-    ).get_app()
+#TO RUN IT AS AN CHAT APPLICATION IN TERMINAL
+multi_agent.cli_app(stream=False, show_full_reasoning=True)
 
-if __name__ == "__main__":
-    serve_playground_app("app:app", reload=True)
+#TO RUN IT AS AN API ON phidata UI
+# multi_agent.print_response("cispy and intresting informatic blog about googles new quantumchip!!", stream=True,show_full_reasoning=True)
+# app = Playground(
+#     agents=[
+#         web_search_agent, 
+#         seo_analyst_agent, 
+#         content_creator_agent, 
+#         editorial_assistant_agent 
+#         ]
+#     ).get_app()
+
+# if __name__ == "__main__":
+#     serve_playground_app(":app", reload=True)
